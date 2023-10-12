6d97c2f7b0a78966678a73b1ac20a64764026088
Md Imran Hossain <imranhossain1402@gmail.com>
2023-10-12 23:00:34 +0200

    Crud on user data completed

---
+++ prisma/migrations/20231012205744_creating_user/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('admin', 'customer');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+    "contact_no" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "profile_img" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
